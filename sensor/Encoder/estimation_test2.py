3374992a64b8ae19781cf2eecb621cb6030e4113
Toshiki-Keio <ftu-0770.txs@keio.jp>
2022-07-18 21:40:00 +0900

    make branch Servomotor

--- sensor/Encoder/estimation_test2.py
+++ sensor/Encoder/estimation_test2.py
@@ -25,6 +25,8 @@ state = 1
 x_remind = []
 y_remind = []
 q_remind = []
+thread1 = []
+thread2 = []
 # MotorR.stop()
 # MotorL.stop()
 
@@ -32,58 +34,40 @@ start_time=time.time()
 print("cansat-x :",x,"[m]")
 print("cansat-y :",y,"[m]")
 print("cansat-q :",q,"[rad]")
-
+bno055.bnoread()
 try:
     print("motor run")
-#     MotorR.stop()
-#     MotorL.stop()
-    x_remind.append(0)
-    y_remind.append(0)
-    bno055.bnoread()
-    q=radians(round(bno055.ex,3))
-    MotorR.go(80)
-    MotorL.go(78)
-#     MotorR.back(60)
-#     MotorL.go(60)
-#     bno055.bnoread()
-    t_new = time.time()
+    MotorR.stop()
+    MotorL.stop()
+    
+    MotorR.go(81)
+    MotorL.go(80)
+#     MotorR.back(80)
+#     MotorL.back(80)
+    
     while True:
-#         t1=time.time()
-        t1 = time.time()
-        cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
-        t2 = time.time()
-        print("odometri time:", t2-t1)
-        #time.sleep(del_t)
-#        t2=time.time()
+        t1=time.time()
+        cansat_speed,cansat_rad_speed, thread1, thread2=Encoder.est_VW(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN, thread1, thread2)
+        t2=time.time()
+        x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
         bno055.bnoread()
-        q=round(bno055.ex,6)
         q=radians(round(bno055.ex,3))
-        t_old = t_new
-        t_new = time.time()
-        x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t_new-t_old,x,y,q)
-        x_remind.append(x)
-        y_remind.append(y)
-        if sqrt((abs(x_remind[-1]-x_remind[0]))**2 + (abs(y_remind[-1]-y_remind[0]))**2) >= 5:
-            MotorR.stop()
-            MotorL.stop()
-            break
         print("cansat speed :",cansat_speed,"[m/s]")
         print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
         print("cansat-x :",x,"[m]")
         print("cansat-y :",y,"[m]")
-        print("cansat-q :",math.degrees(q),"[rad]")
-        
+        print("cansat-q :",q,"[rad]")    
 # bno055.bnoread()
 # try:
 #     print("motor run")
 #     while True:
 #         if state == 1:
 #             q_remind=[]
-#             MotorR.go(86.5)
+#             MotorR.go(85)
 #             MotorL.go(85)
 #             t1=time.time()
 # #             print(0)
-#             cansat_speed,cansat_rad_speed=Encoder.est_v_w(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+#             cansat_speed,cansat_rad_speed, thread1, thread2=Encoder.est_VW(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN, thread1, thread2)
 #             t2=time.time()
 #             x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
 #             bno055.bnoread()
@@ -97,16 +81,20 @@ try:
 #             print("sin",sin(q))
 #             x_remind.append(x)
 #             y_remind.append(y)
-#             if sqrt((abs(x_remind[-1]-x_remind[0]))**2 + (abs(y_remind[-1]-y_remind[0]))**2) >= 10:
+# #             if sqrt((abs(x_remind[-1]-x_remind[0]))**2 + (abs(y_remind[-1]-y_remind[0]))**2) >= 2.5:
+#             if sqrt((abs(x))**2 + (abs(y))**2) >= 2:
 #                 state = 2
+#                 thread1 = []
+#                 thread2 = []
 #         elif state == 2:
 #             x_remind=[]
 #             y_remind = []
 # #             print("motor curve") 
-#             MotorR.go(90)
-#             MotorL.go(30)
+#             MotorR.go(60)
+#             MotorL.go(5)
 #             t1=time.time()
-#             cansat_speed,cansat_rad_speed=Encoder.est_v_w_for_c(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+#             cansat_speed,cansat_rad_speed=Encoder.est_VW_for_c(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN)
+# #             cansat_speed,cansat_rad_speed, thread1, thread2=Encoder.est_VW(ct.const.RIGHT_MOTOR_ENCODER_A_PIN,ct.const.LEFT_MOTOR_ENCODER_A_PIN, thread1, thread2)
 #             t2=time.time()
 #             x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
 #             bno055.bnoread()
@@ -122,8 +110,10 @@ try:
 #             if sin((abs(q_remind[-1]-q_remind[0])))>=0.9:
 #                 state = 1
 #                 """
-#             if fabs(q-90)<3:
+#             if fabs(q-270)<2:
 #                 state=1
+#                 thread1 = []
+#                 thread2 = []
 #         elif state == 3:
 #             MotorR.go(81)
 #             MotorL.go(80)
@@ -144,15 +134,11 @@ try:
         
     #time.sleep(1)
 except KeyboardInterrupt:
-    
+    t2=time.time()
     MotorR.stop()
     MotorL.stop()
-    bno055.bnoread()
-    q=round(bno055.ex,6)
+    x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t2-t1,x,y,q)
     q=radians(round(bno055.ex,3))
-    t_old = t_new
-    t_new = time.time()
-    x,y,q=Encoder.odometri(cansat_speed,cansat_rad_speed,t_new-t_old,x,y,q)
     print("cansat speed :",cansat_speed,"[m/s]")
     print("cansat rad speed :",cansat_rad_speed,"[rad/s]")
     print("cansat-x :",x,"[m]")
@@ -165,3 +151,4 @@ except KeyboardInterrupt:
     GPIO.cleanup()
 
 GPIO.cleanup()
+
--- sensor/radio/2021/RSSI_experiment/motor_test.py
+++ sensor/radio/2021/RSSI_experiment/motor_test.py
@@ -4,7 +4,7 @@ import time
 
 GPIO.setwarnings(False)
 Motor1 = motor.motor(6,5,13)
-# Motor2 = motor.motor(20,16,12)
+Motor2 = motor.motor(20,16,12)
 
 try:
     print("motor run")
@@ -19,15 +19,14 @@ try:
 #             Motor1.go(i)
 #             Motor2.go(i)
 #             time.sleep(0.1)
-        
-        
+                
 #     Motor1.back(100)
 #     Motor2.back(70)
 #     time.sleep(2)
     
-    Motor1.go(30)
-#     Motor2.go(30)
-    time.sleep(10)
+    Motor1.go(80)
+    Motor2.go(80)
+    time.sleep(100)
     
 #     
 #  #   Motor1.back(85)
@@ -39,12 +38,12 @@ try:
 #     #time.sleep(3)
     print("motor stop")
     Motor1.stop()
-#     Motor2.stop()
-    time.sleep(1)
+    Motor2.stop()
+    time.sleep(0.5)
 except KeyboardInterrupt:
     print("motor stop")
     Motor1.stop()
-#     Motor2.stop()
+    Motor2.stop()
     GPIO.cleanup()
 
 GPIO.cleanup()
