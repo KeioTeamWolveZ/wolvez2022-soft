32c9a1d0d8913dcf93ab9700b80054d352fff4ad
Toshiki-Keio <ftu-0770.txs@keio.jp>
2022-08-01 19:30:34 +0900

    change some constant

--- Testcode/EtoE/constant.py
+++ Testcode/EtoE/constant.py
@@ -3,8 +3,8 @@
 
 import const
 
-##Pin Number
-#Motor&Encoder
+## Pin Number
+# Motor&Encoder
 const.RIGHT_MOTOR_IN1_PIN = 6
 const.RIGHT_MOTOR_IN2_PIN = 5
 const.RIGHT_MOTOR_VREF_PIN = 13
@@ -17,46 +17,46 @@ const.LEFT_MOTOR_VREF_PIN = 12
 const.LEFT_MOTOR_ENCODER_A_PIN = 7
 const.LEFT_MOTOR_ENCODER_B_PIN = 8
 
-#LED
+# LED
 const.RED_LED_PIN =  10
 const.BLUE_LED_PIN = 9
 const.GREEN_LED_PIN = 11
 
-#Releasing Pin
+# Releasing Pin
 const.SEPARATION_PIN = 25
 
-#Flight Pin
+# Flight Pin
 const.FLIGHTPIN_PIN = 4
 
-#Motor
+# Motor VREF
 const.LANDING_MOTOR_VREF = 90
 const.SPM_MOTOR_VREF = 70
 const.RUNNING_MOTOR_VREF = 70
 const.STUCK_MOTOR_VREF = 100
 
-##Variable Threshold
+## Variables
 const.GPS_GOAL_LAT = 35.5559391
 const.GPS_GOAL_LON = 139.6525889
-
-
-##State Threshold
-const.PREPARING_GPS_COUNT_THRE= 30
-const.PREPARING_TIME_THRE = 10
-const.FLYING_FLIGHTPIN_COUNT_THRE = 10
-const.DROPPING_ACC_COUNT_THRE = 20
-const.DROPPING_ACC_THRE = 1 #加速度の値
-const.SEPARATION_TIME_THRE = 10 #焼き切り時間
-const.LANDING_CAMERA_TIME_THRE = 2
-
-const.LANDING_MOTOR_TIME_THRE = 5 #分離シートから離れるためにモータを回転させる時間
-const.STUCK_ACC_THRE = 0.1
-const.STUCK_COUNT_THRE = 30
-const.STUCK_MOTOR_TIME_THRE = 5 #分離シートから離れるためにモータを回転させる時間
-const.LANDING_PRE_MOTOR_TIME_THRE = 5 #分離シートから離れるためにモータを回転させる時間
-const.SPMFIRST_PIC_COUNT = 10
 const.f1 = 136
 const.f2 = 196
 const.f3 = 776
-const.STARTING_TIME_THRE = 60
-const.MEASURING_SWITCH_COUNT_THRE = 20 #1地点での測位回数
-const.MEASURING_MAX_MEASURING_COUNT_THRE = 6 #最大測位点
+
+
+## State Threshold
+const.PREPARING_GPS_COUNT_THRE= 30
+const.PREPARING_TIME_THRE = 10
+
+const.FLYING_FLIGHTPIN_COUNT_THRE = 10
+
+const.DROPPING_ACC_COUNT_THRE = 20
+const.DROPPING_ACC_THRE = 1 #加速度の値
+
+const.SEPARATION_TIME_THRE = 10 #焼き切り時間
+
+const.LANDING_CAMERA_TIME_THRE = 2
+const.LANDING_MOTOR_TIME_THRE = 5 #分離シートから離れるためにモータを回転させる時間
+
+const.SPMFIRST_PIC_COUNT = 10
+
+const.STUCK_ACC_THRE = 0.1
+const.STUCK_COUNT_THRE = 30
