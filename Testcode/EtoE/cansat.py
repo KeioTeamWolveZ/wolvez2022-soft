4bcdd0863767628373893efe5e13d41279005b6f
Toshiki-Keio <ftu-0770.txs@keio.jp>
2022-08-09 16:21:50 +0900

    make branch test

--- Testcode/EtoE/cansat.py
+++ Testcode/EtoE/cansat.py
@@ -133,7 +133,7 @@ class Cansat():
         planning_path.close()
 
     def mvfile(self):
-        pre_data = "../../pre_data/*"
+        pre_data = sorted(glob.glob("../../pre_data/*"))
         dest_dir = f"results/{self.startTime}/camera_result/second_spm/learn{self.learncount}"
         for file in pre_data:
             shutil.copy2(file, dest_dir)
