3e43d087dab2e560061feaec7dbf3f79a1bb2e2e
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-08-23 13:13:06 +0900

    bno

--- Testcode/EtoE/bno055.py
+++ Testcode/EtoE/bno055.py
@@ -196,11 +196,13 @@ class BNO055:
     def __init__(self, sensorId=-1, address=0x28):
         self._sensorId = sensorId
         self._address = address
-        self._mode = BNO055.OPERATION_MODE_NDOF
+#         self._mode = BNO055.OPERATION_MODE_NDOF
+        self._mode = BNO055.OPERATION_MODE_COMPASS
 
 
     def begin(self, mode=None):
-        if mode is None: mode = BNO055.OPERATION_MODE_NDOF
+#         if mode is None: mode = BNO055.OPERATION_MODE_NDOF
+        if mode is None: mode = BNO055.OPERATION_MODE_COMPASS
         # Open I2C bus
         self._bus = smbus.SMBus(1)
 
