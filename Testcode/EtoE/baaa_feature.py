b11b0b70aabeae6aa5aa4c0c518885ab90f40f1c
Masato <masato_7121@keio.jp>
2022-08-25 02:38:17 +0900

    Fixed some mathematical mistake

--- Testcode/EtoE/baaa_feature.py
+++ Testcode/EtoE/baaa_feature.py
@@ -186,12 +186,11 @@ class Feature_img():
                 b = float(self.org_img[i][j][0])
                 g = float(self.org_img[i][j][1])
                 r = float(self.org_img[i][j][2])
-                if b < 125:
-                    vari_d = g+r-b
-                    if vari_d != 0:
-                        vari = (g-r)/(g+r-b)
-                        if vari < 0.0:
-                            vari = 0.0
+                vari_d = g+r-b
+                if vari_d != 0 and b < 125:
+                    vari = (g-r)/(g+r-b)
+                    if vari < 0.0:
+                        vari = 0.0
                 else:
                     vari = 0
                 # vari = vari*255/9.0
@@ -203,9 +202,12 @@ class Feature_img():
         #print("vari min: "+str(np.amin(self.vari_list_np)))
         for i in range(self.org_img.shape[0]):
             for j in range(self.org_img.shape[1]):
-                self.vari_list_np[i][j] = 100*(self.vari_list_np[i][j] - vari_min)/(vari_max - vari_min)
-                if self.vari_list_np[i][j] > 1.0:
-                    self.vari_list_np[i][j] = 1.0
+                if vari_max != vari_min:
+                    self.vari_list_np[i][j] = 100*(self.vari_list_np[i][j] - vari_min)/(vari_max - vari_min)
+                    if self.vari_list_np[i][j] > 1.0:
+                        self.vari_list_np[i][j] = 1.0
+                else:
+                    self.vari_list_np[i][j] = 0
                 self.vari_list_np[i][j] = int(255*self.vari_list_np[i][j])
                 # print(self.vari_list_np[i][j])
                 # print(np.uint8(self.vari_list_np[i][j]))
@@ -233,10 +235,10 @@ class Feature_img():
                 b = float(self.org_img[i][j][0])
                 g = float(self.org_img[i][j][1])
                 r = float(self.org_img[i][j][2])
-                if g*g+r*b != 0:
+                if g*g+r*b != 0 and g*g-r*b > 0:
                     rgbvi = (g*g-r*b)/(g*g+r*b)     # ここがGRVIの計算式
                 else:
-                    rgbvi = 0 
+                    rgbvi = 0
                 self.rgbvi_list_np[i][j] = int(rgbvi)
                 self.output_img[i][j] = np.uint8(self.rgbvi_list_np[i][j])
         self.save_name = self.sav_d + f"/rgbvi_{self.frame_num}.jpg"
@@ -254,11 +256,11 @@ class Feature_img():
                 b = float(self.org_img[i][j][0])
                 g = float(self.org_img[i][j][1])
                 r = float(self.org_img[i][j][2])
-                if g+r != 0:
+                if g+r != 0 and g-r > 0:
                     grvi = (g-r)/(g+r)     # ここがGRVIの計算式
                 else:
                     grvi = 0
-                self.grvi_list_np[i][j] = int(grvi)
+                self.grvi_list_np[i][j] = int(255*grvi)
                 self.output_img[i][j] = np.uint8(self.grvi_list_np[i][j])
         self.save_name = self.sav_d + f"/grvi_{self.frame_num}.jpg"
         cv2.imwrite(self.save_name, self.output_img) 
@@ -290,9 +292,9 @@ class Feature_img():
         #self.output_img_list = []
         self.org_img = cv2.imread(self.imp_p, 1)
         self.org_img = cv2.cvtColor(self.org_img, cv2.COLOR_BGR2RGB)
-        kernel = np.array([[0, 2, 0],
-                            [2, -8, 2],
-                            [0, 2, 0]], np.float32)
+        kernel = np.array([[0, 1, 0],
+                            [1, -4, 1],
+                            [0, 1, 0]], np.float32)
         self.output_img = cv2.filter2D(self.org_img, -1, kernel)
         self.save_name = self.sav_d + f"/enphasis_{self.frame_num}.jpg"
         cv2.imwrite(self.save_name, self.output_img)
@@ -304,6 +306,7 @@ class Feature_img():
         self.org_img = cv2.imread(self.imp_p, 1)
         self.img_gray = cv2.cvtColor(self.org_img, cv2.COLOR_BGR2GRAY)
         self.gray=cv2.Canny(self.img_gray,100,200)
+        # self.gray=cv2.Canny(self.img_gray,200,550)  # アーリスではこちらの値を用いた方が適切かも（エッジ検出感度）
         self.save_name = self.sav_d + f"/edge_{self.frame_num}.jpg"
         cv2.imwrite(self.save_name,self.gray)
         self.output_img_list.append(self.save_name)
