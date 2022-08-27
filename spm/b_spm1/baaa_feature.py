909d126f10f04610f1b5d7cb6237621964ca4abb
Masato <masato_7121@keio.jp>
2022-08-27 13:05:36 +0900

    New pre_data

--- spm/b_spm1/baaa_feature.py
+++ spm/b_spm1/baaa_feature.py
@@ -1,6 +1,12 @@
+from pickletools import int4
+from typing import List
 import cv2
+import numpy as np
+import os
+from PIL import Image
+from matplotlib import pyplot as plt
+from time import time
 from FEATURE import Feature_img
-import tempfile
 
 class ReadFeaturedImg():
     """画像読込関数
@@ -10,28 +16,26 @@ class ReadFeaturedImg():
         saveDir (str): Save directory path that allowed tmp
         Save(bool):Save or not, defalt:False
     """
-    def __init__(self, importPath:str=None, saveDir:str=None, Save:bool=False):
+    def __init__(self, importPath:str=None, saveDir:str=None, Save:any=False):
         self.imp_p = importPath
-        if Save:
-            self.sav_d = saveDir
-        #else:
-        #    self.sav_d = tempfile.TemporaryDirectory()
+        self.sav_d = saveDir
         self.save = Save
     
-    def feature_img(self, frame_num, feature_name=None):
+    def feature_img(self, frame_num, feature_names=None):
         '''Change to treated img
         Args:
             frame_num(int):Frame number or time
             feature_name(str):
+        Return:
+            fmg_list(list):List of featured img paths
         '''
         self.treat = Feature_img(self.imp_p, frame_num, self.sav_d)
-        
-        if feature_name == None:
+        if feature_names == None:
             self.treat.normalRGB()
-            #self.treat.vari()
-            #self.treat.rgbvi()
-            #self.treat.grvi()
-            #self.treat.ior()
+#             self.treat.vari()
+#             self.treat.rgbvi()
+#             self.treat.grvi()
+#             self.treat.ior()
             self.treat.enphasis()
             self.treat.edge()
             self.treat.hsv()
@@ -41,36 +45,39 @@ class ReadFeaturedImg():
             self.treat.rb()
             self.treat.gb()
             self.treat.rg()
-        elif feature_name == "normalRGB":
-            self.treat.normalRGB()
-        #elif feature_name == "vari":
-            #self.treat.vari()
-        #elif feature_name == "rgbvi":
-            #self.treat.rgbvi()
-        #elif feature_name == "grvi":
-            #self.treat.grvi()
-        #elif feature_name == "ior":
-            #self.treat.ior()
-        elif feature_name == "enphasis":
-            self.treat.enphasis()
-        elif feature_name == "edge":
-            self.treat.edge()
-        elif feature_name == "hsv":
-            self.treat.hsv()
-        elif feature_name == "r":
-            self.treat.r()
-        elif feature_name == "b":
-            self.treat.b()
-        elif feature_name == "g":
-            self.treat.g()
-        elif feature_name == "rb":
-            self.treat.rb()
-        elif feature_name == "rg":
-            self.treat.rg()
-        elif feature_name == "gb":
-            self.treat.gb()
             
-        
+        else:
+            for feature_name in feature_names:
+                if feature_name == "normalRGB":
+                    self.treat.normalRGB()
+                elif feature_name == "vari":
+                    self.treat.vari()
+                elif feature_name == "rgbvi":
+                    self.treat.rgbvi()
+                elif feature_name == "grvi":
+                    self.treat.grvi()
+                elif feature_name == "ior":
+                    self.treat.ior()
+                elif feature_name == "enphasis":
+                    self.treat.enphasis()
+                elif feature_name == "edge":
+                    self.treat.edge()
+                elif feature_name == "hsv":
+                    self.treat.hsv()
+                elif feature_name == "red":
+                    self.treat.r()
+                elif feature_name == "blue":
+                    self.treat.b()
+                elif feature_name == "green":
+                    self.treat.g()
+                elif feature_name == "purple":
+                    self.treat.rb()
+                elif feature_name == "emerald":
+                    self.treat.gb()
+                elif feature_name == "yellow":
+                    self.treat.rg()
+                else:
+                    self.treat.other()
         fmg_list = self.treat.output()
         
         return fmg_list
@@ -79,6 +86,6 @@ class ReadFeaturedImg():
     def read_img(self, path):
         #print("===== func read_img starts =====")
         self.img=cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-        self.img = self.img[int(0.25*self.img.shape[0]):int(0.75*self.img.shape[0])]
+        self.img = self.img[int(0.225*self.img.shape[0]):int(0.75*self.img.shape[0])]
         # 読み込めないエラーが生じた際のロバスト性も検討したい
         return self.img
