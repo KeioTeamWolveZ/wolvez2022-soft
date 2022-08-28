c3f4c5cfe718891b13d28e6bf7570f5b2f8e5226
kazunosuke1108 <hyperturbo.kazunosuke@gmail.com>
2022-08-28 09:43:52 +0900

    ADD: new analyzer (wip)

---
+++ spm/c_spm2/noshiro_lab/log_analyze/log_analyzer4.py
@@ -0,0 +1,49 @@
+# avoid_test_0827を一括解析
+import numpy as np
+import csv
+import os
+from glob import glob
+from pprint import pprint
+
+basepath=os.getcwd()
+
+path_0827=basepath+"/wolvez2022/spm/c_spm2/noshiro_lab/log_analyze/log/0827"
+
+pprint(sorted(glob(path_0827+"/*/planning_result.txt")))
+
+
+
+no=5
+current_dir=os.getcwd()
+log_path=current_dir+f"/spm/c_spm2/noshiro_lab/log_analyze/log/planning_no{no}.txt"
+csv_path=current_dir+f"/spm/c_spm2/noshiro_lab/log_analyze/results/risk_no{no}_thre.csv"
+"""
+with open(log_path) as f:
+    for i,line in enumerate(f):
+        j=i+1
+        if j%4==1 or j%4==2:
+            data_win=None
+            pass
+        elif j%4==3:
+            idx_s=line.find("Risk:[")+len("Risk:[")
+            idx_e=line.find("]")
+            # print("data : ",line[idx_s:idx_e])
+            data=line[idx_s:idx_e]
+            data_win=data.split(" ")
+            thre_s=line.find("threadshold_risk:")+len("threadshold_risk:")
+            thre_e=line.find(",max_risk:")
+            thre_m_s=line.find(",max_risk:")+len(",max_risk:")
+            thre_m_e=line.find(",boolean_risk:")
+            print("threadshold_risk : ",line[thre_s:thre_e])
+            print("max_risk : ",line[thre_m_s:thre_m_e])
+            data_win.append(line[thre_s:thre_e])
+            data_win.append(line[thre_m_s:thre_m_e])
+            try:
+                data_win.remove("")
+            except ValueError:
+                pass
+        elif j%4==0:
+            with open(csv_path,'a') as f2:
+                writer = csv.writer(f2)
+                writer.writerow(data_win)
+"""
