7f9b3fbac765c36810d97758b82d8dc91637a4cf
Masato <masato_7121@keio.jp>
2022-09-04 13:39:27 +0900

    for merge

--- Testcode/EtoE/load_npz.py
+++ Testcode/EtoE/load_npz.py
@@ -1,19 +1,22 @@
 import numpy as np
 import glob
 
-dir_path = sorted(glob.glob("results/*"))
-print(dir_path)
-dir_path = dir_path[-1] #ここまでが最新時間のフォルダまでのパスとなる．
-print(dir_path)
-file_path = sorted(glob.glob(dir_path + "/camera_result/second_spm/learn1/*")) #planningフォルダ内のnpzファイルまでのパスを取得
-file_path = file_path[-1] #最新のnpzファイルのパスを取得
-print(file_path)
+# dir_path = sorted(glob.glob("results/*"))
+# print(dir_path)
+# dir_path = dir_path[-1] #ここまでが最新時間のフォルダまでのパスとなる．
+# print(dir_path)
+# file_path = sorted(glob.glob(dir_path + "/camera_result/second_spm/learn1/*")) #planningフォルダ内のnpzファイルまでのパスを取得
+# file_path = file_path[-1] #最新のnpzファイルのパスを取得
+# print(file_path)
 
 # file_path = sorted(glob.glob("/home/pi/Desktop/camera_result/planning/learn1/planning_npz/*"))
 # file_path = file_path[-1]
 # print(file_path)
 
-data = np.load(file_path,allow_pickle=True)
-data = data['array_1']
-print(data)
-print(len(data[0]))
+file_path = sorted(glob.glob("../../pre_data_new_14/*.npz"))
+print(file_path)
+for k, path in enumerate(file_path):
+    data = np.load(path,allow_pickle=True)
+    data = data['array_1']
+    print(f'\n\n\n{k}th,   ',data)
+    print(len(data[0]))
